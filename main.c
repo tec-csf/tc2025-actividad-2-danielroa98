ba56d5fb0ecddbea0254d60506aa3fca764615e9
danielroa98 <daniel.roa98@gmail.com>
2020-08-24 07:57:05 -0500

    Created main

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ *  Apuntadores a funciones
+ * 
+ *  Daniel Roa
+ *  A01021960
+ */ 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Libro
+{
+    char * titulo;
+    int paginas;
+};
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
