91c2dcedbf5d3b723c0ef6a4a419571fb8f370e2
danielroa98 <daniel.roa98@gmail.com>
2020-08-25 22:59:59 -0500

    Started value creation

--- main.c
+++ main.c
@@ -7,15 +7,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef void (* seleccion_u)();
+
 typedef struct Libro
 {
     char * titulo;
     int paginas;
-};
-
+}tomo;
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    seleccion_u * opc = (seleccion_u * ) malloc(sizeof(seleccion_u) * 5);
+
+    int numeros,\
+    menu = 0;
+
+    tomo * libro = (tomo *)malloc(sizeof(tomo) * 5);
+
+
     return 0;
 }
