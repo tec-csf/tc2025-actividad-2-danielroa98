c5820474228fd5911fb440bfdf7c33a76bd89b62
danielroa98 <daniel.roa98@gmail.com>
2020-08-27 16:41:10 -0500

    Se creó la inserción de datos

--- main.c
+++ main.c
@@ -7,23 +7,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 5
+
 typedef void (* seleccion_u)();
 
 typedef struct Libro
 {
     char * titulo;
     int paginas;
-}tomo;
+}Libro;
+
+void imprimeInt(int *, int);
+
+void imprimeLibro(Libro *, int);
 
 int main(int argc, char const *argv[])
 {
     seleccion_u * opc = (seleccion_u * ) malloc(sizeof(seleccion_u) * 5);
 
-    int numeros,\
-    menu = 0;
+    int * numeros = (int *) malloc(N * sizeof(int));
+    
+    int * sup = numeros;
+    int * end = numeros + N;
 
-    tomo * libro = (tomo *)malloc(sizeof(tomo) * 5);
+    for (; sup < end; ++sup){
+        *sup = rand() % 100;
+    }
+    
+    printf("\n\nComenzando con los enteros\n\n");
 
+    imprimeInt(numeros, N);
+
+    
+
+    Libro * tomo = (Libro *)malloc(sizeof(Libro) * 5);
+
+    Libro * fin = tomo + 5;
+
+    for (Libro * aux = tomo; aux < fin; ++aux)
+    {
+        Libro * datosLibro = (Libro *)malloc(sizeof(Libro));
+        
+        datosLibro->titulo = (char *)malloc(sizeof(char) * 20);
+        printf("Nombre del libro:");
+        scanf("%s", datosLibro->titulo);
+
+        printf("Número de páginas: ");
+        scanf("%d", &datosLibro->paginas);
+    }
+    
+    imprimeLibro(tomo, N);
 
     return 0;
+}
+
+void imprimeInt(int * numeros, int cont){
+    int * sup = numeros;
+    int * fin = numeros + cont;
+
+    for (; sup < fin; ++sup)
+    {
+        printf(" %d ", *sup);
+    }
+    
+    printf("\n");
+}
+
+void imprimeLibro(Libro * tomo, int contLibros){
+     Libro * sup = tomo;
+     Libro * fin = sup + contLibros;
+
+    printf("\n\tTítulo \t\t No. de páginas");
+
+     for (; sup < fin; ++sup){
+         printf("\t%s \t\t %d", sup->titulo, sup->paginas);
+     }
+     
+    printf("\n");
 }
